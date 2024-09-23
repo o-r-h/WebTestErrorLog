a49dbf60165adbf2dfc3acf7b844b68d644fd429
Omar Rodriguez <orodriguez@peruapps.com.pe>
2024-09-23 09:20:13 -0500

    cambios menores

--- WebTestErrorLog/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebTestErrorLog.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- WebTestErrorLog/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebTestErrorLog.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
