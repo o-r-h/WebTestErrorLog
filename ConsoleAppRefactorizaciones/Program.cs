a4525ab25dc2b088a44c99ca8208281db4a12ced
Omar Rodriguez <orodriguez@peruapps.com.pe>
2024-09-25 10:52:10 -0500

    cambio de nombres

--- ConsoleAppRefactorizaciones/Program.cs
+++ ConsoleAppRefactorizaciones/Program.cs
@@ -22,19 +22,33 @@ namespace ConsoleAppRefactorizaciones
 			var processor = new Processor(verificarUsuario, guardarFoto, datosReniec);
 			var request1 = new ProcessRequest
 			{
-				RequestObtenerBase = new Clases.RequestObtenerBase{ Token ="99999", Usuario="Pedro" },
+				RequestObtenerBase = new Clases.RequestObtenerBase{ Token ="99999", Usuario="Jose" },
 				requestConsultarReniec = new Clases.RequestConsultarReniec{ NumeroDocumento ="44212332", Pais ="604", TipoDocumento ="21" },
 			    foto = new Clases.RequestFoto{ Extension="jpg", LongitudByte=5000000, Nombre ="Cliente001"},
 				Alfa = new Alfa(),
 				Beta = new Beta(),
 				Gamma = new Gamma()
 			};
-			Console.WriteLine("Iniciando procesamiento Alfa -> Beta -> Delta:");
+			Console.WriteLine("Iniciando procesamiento validarusuario -> consultar Reniec -> GuardarFoto:");
+			Console.WriteLine("\n-----------------------------------------------------------------------\n");
 			processor.Process(request1, "ProcesoGuardarFotoReniec");
 
 			Console.WriteLine("\n---------------------------------\n");
-			Console.ReadLine();
+			Console.WriteLine("Fin");
+			Console.WriteLine("");
 
+			var request2 = new ProcessRequest
+			{
+				Alfa = new Alfa(),
+				Beta = new Beta(),
+				Gamma = new Gamma()
+			};
+			Console.WriteLine("Iniciando procesamiento alfa -> beta -> gamma:");
+			Console.WriteLine("\n-----------------------------------------------------------------------\n");
+			processor.Process(request2, "EjemploBasico");
+			Console.WriteLine("\n---------------------------------\n");
+			Console.WriteLine("Fin");
+			Console.ReadLine();
 		}
 
 
